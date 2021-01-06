4093fd9c701419a640086424c506fbf25fe85769
Pyseph <45090858+flareo@users.noreply.github.com>
2021-01-06 18:42:24 +0100

    Update Source.lua
    Removed debugs

--- Source.lua
+++ Source.lua
@@ -6,15 +6,11 @@ function heap.insert(value, data)
 		value = value,
 		data = data
 	}
-	--print('Adding', value)
-
 	local parentNode = heap[insertPos]
 	local childNode = heap[math.floor(insertPos / 2)]
 	-- if parent has less time left than child, swap parent and child
 	-- node with least time left will be at the end
 	while insertPos > 1 and parentNode.data[2] - parentNode.value < childNode.data[2] - childNode.value do
-		--print('swapping parent', heap[insertPos].value, '(' .. insertPos .. ')', 'with child', heap[math.floor(insertPos / 2)].value, '(' .. math.floor(insertPos / 2) .. ')')
-
 		local childPos = math.floor(insertPos / 2)
 		heap[insertPos], heap[childPos] = heap[childPos], heap[insertPos]
 		insertPos = math.floor(insertPos / 2)
@@ -23,37 +19,27 @@ end
 function heap.extract()
 	local insertPos = 1
 	if #heap < 2 then
-		--print('heap only has 1 index')
 		heap[1] = nil
 		return
 	end
-	--print('removed last element')
 	heap[1] = table.remove(heap)
-
-	--print('insertPos and #heap are:', insertPos, #heap)
-	--print('unsorted heap is:', heap)
+	
 	while insertPos < #heap do
 		local childL, childR = heap[2*insertPos], heap[2*insertPos+1]
-		--print('getting next children', 2*insertPos, 'and', 2*insertPos+1)
 		if not childL or not childR then
-			--print('no such children', childL, childR)
 			break
 		end
 
 		local smallerChild = 2*insertPos + (childL.data[2] - childL.value < childR.data[2] - childR.value and 0 or 1)
-		--print('the smaller child is', smallerChild)
 
 		local child = heap[smallerChild]
 		local parent = heap[insertPos]
 
-		--print('the nodes are', child, parent)
 		if parent.data[2] - parent.value > child.data[2] - child.value then
-			--print('swapping the two')
 			heap[smallerChild], heap[insertPos] = parent, child
 		end
 		insertPos = smallerChild
 	end
-	--print('sorted heap is:', heap)
 end
 
 local CPUTime = os.clock()
