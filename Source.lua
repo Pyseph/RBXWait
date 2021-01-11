63e3dca041ff2cbaa1599873bf4d43b399e4bc8d
Pyseph <45090858+PysephRBX@users.noreply.github.com>
2021-01-11 19:29:15 +0100

    Update Source.lua
    Addressed issues where calculating the most-prioritized yield did not take into account for current time. This means that this script:
    ```lua
    spawn(function()
    	while true do
    		print("a")
    		BetterWait(0.1)
    	end
    end)
    
    BetterWait(1)
    print("b")
    ```
    Would never get to "b", because the 0.1 always has most priority.

--- Source.lua
+++ Source.lua
@@ -12,7 +12,7 @@ function heap.insert(value, data)
 	local childNode = heap[math.floor(insertPos / 2)]
 	-- if parent has less time left than child, swap parent and child
 	-- node with least time left will be at the end
-	while insertPos > 1 and parentNode.data[2] - parentNode.value > childNode.data[2] - childNode.value do
+	while insertPos > 1 and os.clock() - parentNode.data[2] - parentNode.value > os.clock() - childNode.data[2] - childNode.value do
 		--print('swapping parent', heap[insertPos].value, '(' .. insertPos .. ')', 'with child', heap[math.floor(insertPos / 2)].value, '(' .. math.floor(insertPos / 2) .. ')')
 
 		local childPos = math.floor(insertPos / 2)
@@ -40,14 +40,14 @@ function heap.extract()
 			break
 		end
 
-		local smallerChild = 2*insertPos + (childL.data[2] - childL.value < childR.data[2] - childR.value and 0 or 1)
+		local smallerChild = 2*insertPos + (os.clock() - childL.data[2] - childL.value < os.clock() - childR.data[2] - childR.value and 0 or 1)
 		--print('the smaller child is', smallerChild)
 
 		local child = heap[smallerChild]
 		local parent = heap[insertPos]
 
 		--print('the nodes are', child, parent)
-		if parent.data[2] - parent.value < child.data[2] - child.value then
+		if os.clock() - parent.data[2] - parent.value < os.clock() - child.data[2] - child.value then
 			--print('swapping the two')
 			heap[smallerChild], heap[insertPos] = parent, child
 		end
