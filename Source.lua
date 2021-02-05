95686b932a15889692a60b5905acc6e260eb7ab7
Pyseph <45090858+flareo@users.noreply.github.com>
2021-02-05 22:03:18 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -6,74 +6,68 @@ function heap.insert(value, data)
 		value = value,
 		data = data
 	}
-	--print('Adding', value)
 
-	local parentNode = heap[insertPos]
-	local childNode = heap[math.floor(insertPos / 2)]
+	local parentNode = heap[#heap]
+	local childNode = heap[math.floor(#heap / 2)]
 	-- if parent has less time left than child, swap parent and child
 	-- node with least time left will be at the end
-	while insertPos > 1 and os.clock() - parentNode.data[2] - parentNode.value > os.clock() - childNode.data[2] - childNode.value do
-		--print('swapping parent', heap[insertPos].value, '(' .. insertPos .. ')', 'with child', heap[math.floor(insertPos / 2)].value, '(' .. math.floor(insertPos / 2) .. ')')
 
-		local childPos = math.floor(insertPos / 2)
-		heap[insertPos], heap[childPos] = heap[childPos], heap[insertPos]
-		insertPos = math.floor(insertPos / 2)
+	local startTime = time()
+	while #heap > 1 and startTime - parentNode.data[2] - parentNode.value > startTime - childNode.data[2] - childNode.value do
+		local childPos = math.floor(#heap / 2)
+		heap[#heap], heap[childPos] = heap[childPos], heap[#heap]
+		insertPos = childPos
 	end
+	if data[4] == 2
 end
+
 function heap.extract()
 	local insertPos = 1
-	if #heap < 2 then
-		--print('heap only has 1 index')
-		heap[1] = nil
-		return
-	end
-	--print('removed last element')
-	heap[1] = table.remove(heap)
+	heap[1], heap[#heap] = heap[#heap], nil
 
-	--print('insertPos and #heap are:', insertPos, #heap)
-	--print('unsorted heap is:', heap)
+	local startTime = time()
 	while insertPos < #heap do
 		local childL, childR = heap[2*insertPos], heap[2*insertPos+1]
-		--print('getting next children', 2*insertPos, 'and', 2*insertPos+1)
-		if not childL or not childR then
-			--print('no such children', childL, childR)
+		if not (childL and childR) then
 			break
 		end
 
-		local smallerChild = 2*insertPos + (os.clock() - childL.data[2] - childL.value < os.clock() - childR.data[2] - childR.value and 0 or 1)
-		--print('the smaller child is', smallerChild)
+		local smallerChild = 2*insertPos + (startTime - childL.data[2] - childL.value < startTime - childR.data[2] - childR.value and 0 or 1)
 
 		local child = heap[smallerChild]
 		local parent = heap[insertPos]
 
-		--print('the nodes are', child, parent)
-		if os.clock() - parent.data[2] - parent.value < os.clock() - child.data[2] - child.value then
-			--print('swapping the two')
+		if startTime - parent.data[2] - parent.value < startTime - child.data[2] - child.value then
 			heap[smallerChild], heap[insertPos] = parent, child
 		end
 		insertPos = smallerChild
 	end
-	--print('sorted heap is:', heap)
 end
 
 game:GetService('RunService').Stepped:Connect(function()
 	local PrioritizedThread = heap[1]
+	if not PrioritizedThread then
+		return
+	end
 
-	while PrioritizedThread do
-		PrioritizedThread = PrioritizedThread.data
-		local YieldTime = os.clock() - PrioritizedThread[2]
-		if PrioritizedThread[3] - YieldTime <= 0 then
-			--print('unyielded', PrioritizedThread[3], os.clock(), PrioritizedThread[2], YieldTime)
+	local startTime = time()
+	for _ = 1, 100 do
+		local threadData = PrioritizedThread.data
+		local YieldTime = startTime - threadData[2]
+		if threadData[3] - YieldTime <= 0 then
 			heap.extract()
-			coroutine.resume(PrioritizedThread[1], YieldTime)
+			coroutine.resume(threadData[1], YieldTime, startTime)
 			PrioritizedThread = heap[1]
+			if not PrioritizedThread then
+				break
+			end
 		else
-			PrioritizedThread = nil
+			break
 		end
 	end
 end)
 
-return function(Time)
-	heap.insert(Time or 0, {coroutine.running(), os.clock(), Time or 0})
-	return coroutine.yield(), os.clock()
+return function(Time, Index)
+	heap.insert(Time or 0, {coroutine.running(), time(), Time or 0, Index})
+	return coroutine.yield()
 end
