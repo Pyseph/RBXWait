24a210f2cce4d8cd757368ac428ba5f38878c756
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2021-08-12 00:37:10 -0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,7 +1,10 @@
+local IS_DEFERRED = false
+
+local t_insert = table.insert
 local o_clock = os.clock
 local c_yield = coroutine.yield
 local c_running = coroutine.running
-local c_resume = coroutine.resume
+local runThread = IS_DEFFERED and task.defer or task.spawn
 
 local Yields = {}
 game:GetService('RunService').Stepped:Connect(function()
@@ -10,13 +13,13 @@ game:GetService('RunService').Stepped:Connect(function()
 		local Spent = Clock - data[1]
 		if Spent >= data[2] then
 			Yields[Idx] = nil
-			c_resume(data[3], Spent, Clock)
+			runThread(data[3], Spent, Clock)
 		end
 	end
 end)
 
 return function(Time)
 	Time = (type(Time) ~= 'number' or Time < 0) and 0 or Time
-	table.insert(Yields, {o_clock(), Time, c_running()})
+	t_insert(Yields, {o_clock(), Time, c_running()})
 	return c_yield()
 end
